41532212d5c9726102ab1b3ac64fc4ed7e85a370
Kevin Selling <kevin.selling@hotmail.com>
2024-05-24 22:26:30 +0200

    vercel.conf is back

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+  },
+  assetsInclude: ["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.svg"], 
+});
