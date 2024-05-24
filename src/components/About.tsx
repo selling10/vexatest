818a8fd2f0dc72526090dc34b080c4b079693683
Kevin Selling <kevin.selling@hotmail.com>
2024-05-24 23:38:01 +0200

    wipssos

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -60,23 +60,21 @@ export const About = () => {
   return (
     <section id="about" className="py-16 sm:py-24 bg-[#EFE3E3] w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="bg-muted/50 border rounded-lg py-12 px-6">
+        <div className="bg-[#EFE3E3] py-12 px-6">
           <div className="flex flex-col gap-8 md:gap-12">
-            <div className="bg-green-0 flex flex-col justify-between">
+            <div className="flex flex-col justify-between">
               <div className="pb-6">
-                <h2 className="text-3xl md:text-4xl font-bold">
-                  Om oss
-                </h2>
-                <p className="text-xl text-muted-foreground mt-4">
-                Sedan vår start 2019 har vi fokuserat på förvaltning av fastigheter och sale-leaseback affärer. Vi har över fem års erfarenhet i fastighetsbranschen och vårt mål är att hjälpa företag att växa genom att frigöra kapital och minska kostnader. Genom att erbjuda skräddarsydda finansiella lösningar ger vi företag möjligheten att fokusera på sin kärnverksamhet.                </p>
+                <h2 className="text-3xl md:text-4xl font-bold text-black">Om oss</h2>
+                <p className="text-xl mt-4 text-black">
+                  Sedan vår start 2019 har vi fokuserat på förvaltning av fastigheter och sale-leaseback affärer. Vi har över fem års erfarenhet i fastighetsbranschen och vårt mål är att hjälpa företag att växa genom att frigöra kapital och minska kostnader. Genom att erbjuda skräddarsydda finansiella lösningar ger vi företag möjligheten att fokusera på sin kärnverksamhet.
+                </p>
               </div>
               <Statistics />
             </div>
-            <div className="bg-green-0 flex flex-col justify-between">
-             
+            <div className="flex flex-col justify-between">
               <div className="grid md:grid-cols-2 gap-8">
                 {teamList.map(({ imageUrl, name, position, email, phone, socialNetworks }: TeamProps) => (
-                  <Card key={name} className="bg-white bg-opacity-90 rounded-lg shadow-md overflow-hidden">
+                  <Card key={name} className="bg-white rounded-lg shadow-md overflow-hidden">
                     <CardHeader className="flex flex-col items-center pt-8 pb-4">
                       <img
                         src={imageUrl}
