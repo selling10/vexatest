8ec0961716e5f47d49a294082686cc4bdbb110c1
Kevin Selling <kevin.selling@hotmail.com>
2024-05-24 15:23:43 +0200

    New update1

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import { About } from "./components/About";
-import { Cta } from "./components/Cta";
 import { FAQ } from "./components/FAQ";
-import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
-import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
 import { RentCalculator } from "./components/RentCalculator";
-import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
 import { ApplicationForm } from "./components/ApplicationForm";
 import { HowWorks } from "./components/HowWorks";
-import { Team } from "./components/Team";
-import { Testimonials } from "./components/Testimonials";
 import "./App.css";
 
 function App() {
