ac45b637032a74de910cbf331b3ec15775a9cf95
Kevin Selling <kevin.selling@hotmail.com>
2024-06-09 22:05:07 +0200

    new adress

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,20 +8,21 @@ export const Footer = () => {
 
           </div>
           <div className="mb-2 md:mb-0">
-            <h1 className="text-lg font-bold">Vexa Industrihus</h1>
+            <h1 className="text-lg font-bold">Vexa Industrihus, en del av Utvide Fastigheter AB</h1>
+            <p>Org. Nummer: 559044-8337</p>
             <p>Adress: Grev Turegatan 26, 114 38 Stockholm</p>
           </div>
           <div className="mb- md:mb-0">
             <h2 className="font-bold">Kontaktuppgifter</h2>
             <p>E-post: info@vexaindustrihus.se</p>
-            <p>Telefon: +46123456799</p>
+  
           </div>
           <div>
 
           </div>
         </div>
         <div className="mt-8 text-center text-sm text-gray-600">
-          &copy; {new Date().getFullYear()} by Vexa Industrihus. All rights reserved.
+          &copy; {new Date().getFullYear()} by Utvide Fastigheter AB. All rights reserved.
         </div>
       </div>
     </footer>
