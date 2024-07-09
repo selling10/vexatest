058b2e67e325901c1f303fd5c75849a0bbe592ef
Kevin Selling <kevin.selling@hotmail.com>
2024-07-09 10:05:49 +0200

    new hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,11 +12,11 @@ export const Hero = () => {
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       <div className="relative z-10 text-center">
         <main className="text-5xl md:text-6xl font-bold text-white">
-          <h1>Vi köper din fastighet till marknadsvärde</h1>
+          <h1>Frigör kapitalet i din fastighet!</h1>
         </main>
 
         <div className="text-2xl text-white md:w-10/12 mx-auto mt-4">
-          <h2>Frigör kapitalet i din industrifastighet, och fokusera på din verksamhet.</h2>
+          <h2>Vi köper din fastighet till marknadsvärde.</h2>
         </div>
         <div className="text-2xl text-white md:w-10/12 mx-auto mt-2">
           <h3>Enkelt, tryggt och långsiktigt.</h3>
